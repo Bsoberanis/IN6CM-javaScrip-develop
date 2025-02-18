fc17ed44507f42e26a082c2d6801d5f37198e07a
Bsoberanis <bsoberanis-20233433@kinal.edu.gt>
2025-02-18 17:27:44 -0600

    errores coregidos

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 import { config } from "dotenv";
 config();
 
 import { initServer } from './configs/server.js';
 initServer();
-=======
-import { config } from 'dotenv';
-config();
-
-import {iniciarServidor} from './configs/server.js';
-
-iniciarServidor();
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
