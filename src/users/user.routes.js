9536117cf17c3c1caa5a398eb6f1417c1182359f
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 10:32:17 -0600

    corrección de multiplicación de información

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,28 +1,11 @@
 import { Router } from "express";
 import { check } from "express-validator";
-<<<<<<< HEAD
-import { deleteUser, getUserById, getUsers, updateUser } from "./user.controller.js";
-import { existeUsuarioById } from "../helpers/db-validator.js";
-import { validarCampos } from "../middlewares/validar-campos.js";
-import { uploadProfilePicture } from "../middlewares/multer-upload.js";
-import { tieneRole } from "../middlewares/validar-roles.js";
-import {validarJWT} from "../middlewares/validar-jwt.js";
-
-const router = Router();
-
-router.get("/",getUsers);
-
-router.get(
-    "/findUser/:id", 
-    [
-        check("id","No es un ID valido").isMongoId(),
-=======
 import { getUsers, getUserById, updateUser, deleteUser } from "./user.controller.js";
 import { existeUsuarioById } from "../helpers/db-validator.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
 import { uploadProfilePicture } from "../middlewares/multer-upload.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
-import { tieneRole } from "../middlewares/validar-roles.js"
+import { tieneRole } from "../middlewares/validar-roles.js";
 
 const router = Router();
 
@@ -32,53 +15,33 @@ router.get(
     "/findUser/:id",
     [
         check("id", "No es un ID válido").isMongoId(),
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
         check("id").custom(existeUsuarioById),
         validarCampos
     ],
     getUserById
-<<<<<<< HEAD
 );
 
 router.put(
-    '/:id',
-    uploadProfilePicture.single('profilePicture'),
-    [
-        check("id","No es un ID valido").isMongoId(),
-=======
-)
-
-router.put(
     "/:id",
-    uploadProfilePicture.single('profilePicture'),
+    uploadProfilePicture.single("profilePicture"),
     [
         check("id", "No es un ID válido").isMongoId(),
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
         check("id").custom(existeUsuarioById),
         validarCampos
     ],
     updateUser
-<<<<<<< HEAD
 );
-=======
-)
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
 
 router.delete(
     "/:id",
     [
         validarJWT,
-<<<<<<< HEAD
-        tieneRole("ADMIN_ROLE","VENTAS_ROLE"),
-        check("id","No es un ID valido").isMongoId(),
-=======
         tieneRole("ADMIN_ROLE", "VENTAS_ROLE"),
         check("id", "No es un ID válido").isMongoId(),
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
         check("id").custom(existeUsuarioById),
         validarCampos
     ],
     deleteUser
-)
+);
 
 export default router;
