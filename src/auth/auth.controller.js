f4e718e853580c9e75c8d8e6b449537f33ce1d72
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 09:36:09 -0600

    corrigiendo multiplicación de información

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,86 +1,13 @@
-<<<<<<< HEAD
 import { hash, verify } from 'argon2';
 import Usuario from '../users/user.model.js';
-import  {generarJWT} from '../helpers/generate-jwt.js';
-
-export const register = async(req, res) => {
-
-    try {
-        const data = req.body;
-        
-        let profilePicture = req.file ? req.file.filename : null;
-        const encryptedPassword = await hash (data.password);
-        
-=======
-import Usuario from '../users/user.model.js';
-import { hash, verify } from 'argon2';
-import { generarJWT} from '../helpers/generate-jwt.js';
-
-export const login = async (req, res) => {
-
-    const { email, password, username } = req.body;
-
-    try {
-        
-        const lowerEmail = email ? email.toLowerCase() : '';
-        const lowerUsername = username ? username.toLowerCase() : '';
-
-        const user = await Usuario.findOne({
-            $or: [{ email: lowerEmail }, { username: lowerUsername }]
-        });
-
-        console.log(lowerUsername)
-
-        if(!user){
-            return res.status(400).json({
-                msg: 'Credenciales incorrectas, Correo no existe en la base de datos'
-            });
-        }
-
-        if(!user.estado){
-            return res.status(400).json({
-                msg: 'El usuario no existe en la base de datos'
-            });
-        }
-
-        const validPassword = await verify(user.password, password);
-        if(!validPassword){
-            return res.status(400).json({
-                msg: 'La contraseña es incorrecta'
-            });
-        }
-
-        const token = await generarJWT( user.id );
-
-        return res.status(200).json({
-            msg: 'Inicio de sesión exitoso!!',
-            userDetails: {
-                username: user.username,
-                token: token,
-                profilePicture: user.profilePicture
-            }
-        })
-
-    } catch (e) {
-        
-        console.log(e);
-
-        return res.status(500).json({
-            message: "Server error",
-            error: e.message
-        })
-    }
-}
+import { generarJWT } from '../helpers/generate-jwt.js';
 
 export const register = async (req, res) => {
     try {
         const data = req.body;
-
         let profilePicture = req.file ? req.file.filename : null;
+        const encryptedPassword = await hash(data.password);
 
-        const encryptedPassword = await hash (data.password);
-
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
         const user = await Usuario.create({
             name: data.name,
             surname: data.surname,
@@ -90,15 +17,14 @@ export const register = async (req, res) => {
             password: encryptedPassword,
             role: data.role,
             profilePicture
-        })
+        });
 
         return res.status(201).json({
-<<<<<<< HEAD
-            message: "User registred succesfully",
-            userDetails:{
+            message: "User registered successfully",
+            userDetails: {
                 user: user.email
             }
-        })
+        });
     } catch (error) {
         console.log(error);
         return res.status(500).json({
@@ -106,105 +32,89 @@ export const register = async (req, res) => {
             error: error.message
         });
     }
-}
+};
 
-export const login = async(req, res) => {
-    console.log("hola")
-    const {email, password, username} = req.body;
-    
+export const login = async (req, res) => {
+    const { email, password, username } = req.body;
+
     try {
-        const lowerEmail = email ? email.toLowerCase(): null;
-        const lowerUsername= username ? username.toLowerCase(): null;
+        const lowerEmail = email ? email.toLowerCase() : '';
+        const lowerUsername = username ? username.toLowerCase() : '';
 
         const user = await Usuario.findOne({
-            $or: [{email: lowerEmail}, {username: lowerUsername}]
+            $or: [{ email: lowerEmail }, { username: lowerUsername }]
         });
-        if(!user){
-            return res.status(400).json({
-                msg: 'Credenciales incorrectas, el correo no esta registrado'
-            });
-        }   
 
-        if(!user.estado){
-            return  res.status(400).json({
+        if (!user) {
+            return res.status(400).json({
+                msg: 'Credenciales incorrectas, el correo no está registrado'
+            });
+        }
+
+        if (!user.estado) {
+            return res.status(400).json({
                 msg: 'El usuario no existe en la base de datos'
             });
         }
 
-        const validPassword= await verify(user.password,password);
-        if(!validPassword){
-            return res.status(400).json({
-                msg: 'La contrasena es incorrecta'
-            });
-        }
-
-        const token = await generarJWT(user.id);
-
-        res.status(200).json({
-            msg: 'Login OK',
-            userDetails:{
-                username: user.username,
-                token: token,
-                profilePicture: user.profilePicture
-            }
-        })
-
-
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            msg: "Comuniquese con el administrador",
-            error: e.message
-        })
-    }
-}
-
-export const updatePassword = async (req, res) => {
-    const { id } = req.params;
-    const { password, email, oldPassword, username, ...data } = req.body;
-
-    try {
-        // Validar y transformar el email y el username si se proporcionan
-        const lowerEmail = email ? email.toLowerCase() : null;
-        const lowerUsername = username ? username.toLowerCase() : null;
-
-        console.log("BP1");
-
-        // Buscar el usuario por email o username
-        const user = await Usuario.findOne({
-            $or: [{ email: lowerEmail }, { username: lowerUsername }]
-        });
-        console.log("BP2");
-
-        if (!user) {
-            return res.status(404).json({
-                msg: 'No se encontró al usuario'
-            });
-        }
-        console.log("BP3");
-
-        // Verificar la contraseña anterior
-        const validPassword = await verify(user.password, oldPassword);
-        console.log("BP4");
-
+        const validPassword = await verify(user.password, password);
         if (!validPassword) {
             return res.status(400).json({
                 msg: 'La contraseña es incorrecta'
             });
         }
 
-        // Validar que se proporcione una nueva contraseña
+        const token = await generarJWT(user.id);
+
+        return res.status(200).json({
+            msg: 'Inicio de sesión exitoso!!',
+            userDetails: {
+                username: user.username,
+                token: token,
+                profilePicture: user.profilePicture
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            message: "Server error",
+            error: e.message
+        });
+    }
+};
+
+export const updatePassword = async (req, res) => {
+    const { id } = req.params;
+    const { password, email, oldPassword, username, ...data } = req.body;
+
+    try {
+        const lowerEmail = email ? email.toLowerCase() : null;
+        const lowerUsername = username ? username.toLowerCase() : null;
+
+        const user = await Usuario.findOne({
+            $or: [{ email: lowerEmail }, { username: lowerUsername }]
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                msg: 'No se encontró al usuario'
+            });
+        }
+
+        const validPassword = await verify(user.password, oldPassword);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'La contraseña es incorrecta'
+            });
+        }
+
         if (!password) {
             return res.status(400).json({
                 msg: 'La nueva contraseña es requerida.'
             });
         }
 
-        // Hacer el hash de la nueva contraseña
         const hashPassword = await hash(password);
-        console.log("BP5");
-
-        // Actualizar el usuario con la nueva contraseña
         const updatedUser = await Usuario.findByIdAndUpdate(id, {
             ...data,
             password: hashPassword
@@ -216,7 +126,6 @@ export const updatePassword = async (req, res) => {
             });
         }
 
-        // Devolver la respuesta exitosa
         return res.status(200).json({
             success: true,
             msg: 'Contraseña actualizada correctamente',
@@ -230,23 +139,4 @@ export const updatePassword = async (req, res) => {
             error: error.message
         });
     }
-}
-=======
-            message: "User registered successfully",
-            userDetails: {
-                user: user.email
-            }
-        });
-
-    } catch (error) {
-        
-        console.log(error);
-
-        return res.status(500).json({
-            message: "User registration failed",
-            error: err.message
-        })
-
-    }
-}
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
+};
