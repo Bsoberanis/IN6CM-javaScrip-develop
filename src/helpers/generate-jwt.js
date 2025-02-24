cda810e429cf7c43bc98454ca4d44078c21e7450
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 10:23:00 -0600

    corrigiendo multiplicación de información

--- src/helpers/generate-jwt.js
+++ src/helpers/generate-jwt.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 import jwt from "jsonwebtoken";
 
 export const generarJWT = (uid = ' ') => {
-    return new Promise((resolve, reject)=>{
-        
-=======
-import jwt from 'jsonwebtoken';
-
-export const generarJWT = (uid = ' ') => {
-
-    return new Promise((resolve, reject)=>{
-
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
+    return new Promise((resolve, reject) => {
         const payload = { uid };
 
         jwt.sign(
@@ -21,11 +11,7 @@ export const generarJWT = (uid = ' ') => {
                 expiresIn: '1h'
             },
             (err, token) => {
-<<<<<<< HEAD
-                err ? (console.log(err), reject('no se pudo generar el token')) : resolve(token);
-=======
                 err ? (console.log(err), reject('No se pudo generar el token')) : resolve(token);
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
             }
         );
     });
