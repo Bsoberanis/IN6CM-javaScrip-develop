889ad1f020660992d2b3d43fb3de6aab4a3971f6
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 10:23:33 -0600

    corrigiendo multiplicación de información

--- src/middlewares/delete-file-on-error.js
+++ src/middlewares/delete-file-on-error.js
@@ -1,29 +1,16 @@
 import fs from 'fs/promises';
 import { join } from 'path';
 
-<<<<<<< HEAD
-export const deleteFileonError = async (err, req, res, next)=>{
-    if(req.file && req.filePath){
+export const deleteFileOnError = async (err, req, res, next) => {
+    if (req.file && req.filePath) {
         const filePath = join(req.filePath, req.file.filename);
         try {
             await fs.unlink(filePath);
         } catch (unlinkErr) {
-            console.error(`Error deleting file: `, unlinkErr)
+            console.error('Error deleting file: ', unlinkErr);
         }
     }
-    if(err.status === 400 || err.errors){
-=======
-export const deleteFileOnError = async (err, req, res, next) => {
-    if(req.file && req.filePath){
-        const filePath = join (req.filePath, req.file.filename);
-        try {
-            await fs.unlink(filePath);
-        } catch (unlinkErr) {
-            console.error('Error deleting file: ', unlinkErr)
-        }
-    }
-    if(err.status === 400 || err.errors) {
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
+    if (err.status === 400 || err.errors) {
         return res.status(400).json({
             success: false,
             errors: err.errors
@@ -32,5 +19,5 @@ export const deleteFileOnError = async (err, req, res, next) => {
     return res.status(500).json({
         success: false,
         message: err.message
-    })
+    });
 }
