afbc1c649af3c5a2a8e59264dfb894ff738439ca
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 10:24:14 -0600

    corrigiendo multiplicación de información

--- src/middlewares/multer-upload.js
+++ src/middlewares/multer-upload.js
@@ -4,37 +4,12 @@ import { fileURLToPath } from "url";
 
 const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
 const MIMETYPES = ["image/jpeg", "image/png", "image/jpg"];
-<<<<<<< HEAD
-const MAX_SIZE = 10000000
-=======
 const MAX_SIZE = 10000000;
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
 
 const createMulterConfig = (destinationPath) => {
     return multer({
         storage: multer.diskStorage({
             destination: (req, file, cb) => {
-<<<<<<< HEAD
-                const fullPath = join(CURRENT_DIR, destinationPath)
-                req.filePath = fullPath;
-                cb(null, fullPath);
-            },
-
-            filename: (req, file, cb) => {
-                const fileExtension = extname(file.originalname);
-                const fileName = file.originalname.split(fileExtension)[0];
-                cb(null, `${fileName}-${Date.now()}${fileExtension}`)
-            }
-        }),
-
-        fileFilter: (req, file, cb) => {
-            if (MIMETYPES.includes(file.mimetypes)) cb(null);
-            else cb(new Error(`Only ${MIMETYPES.join(" ")} mimetypes are allowed`));
-        
-        }
-    });
-}
-=======
                 const fullPath = join(CURRENT_DIR, destinationPath);
                 req.filePath = fullPath; // Set the filePath on the request object
                 cb(null, fullPath);
@@ -54,7 +29,6 @@ const createMulterConfig = (destinationPath) => {
         },
     });
 };
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
 
 export const uploadProfilePicture = createMulterConfig("../public/uploads/profile-pictures");
 export const uploadPetPicture = createMulterConfig("../public/uploads/pet-pictures");
