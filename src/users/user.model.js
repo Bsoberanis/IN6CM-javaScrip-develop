7c96d569dc0ccba107649824951298b5f1114c23
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 10:31:42 -0600

    corrección de multiplicación de información

--- src/users/user.model.js
+++ src/users/user.model.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-import {Schema, model} from "mongoose";
-
-const UserSchema = Schema({
-    name: {
-        type: String,
-        required: [true,"Name is required."],
-        maxLenght: [25,"Cannot be overcome 25 characters"]
-    },
-    surname:{
-        type: String,
-        required: [true,"Surname is required."],
-        maxLenght: [25,"Cannot be overcome 25 characters"]
-    },
-    username:{
-        type: String,
-        unique: true
-    },
-    email: {
-        type: String,
-        required:[true, "Email is required"],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, "La contrasena es requerida"],
-        minLenght: [8, "Cannot overcome 8 characters"]
-    },
-    profilePicture: {
-        type: String
-    },
-    phone:{
-        type: String,
-        minLenght: 8,
-        maxLenght: 8,
-        required: true,
-    },
-    role:{
-        type: String,
-        required: true,
-        enum: ["ADMIN_ROLE","USER_ROLE"]
-    },
-    estado:{
-        type: Boolean,
-        default: true
-    }
-},
-    {
-        timestamps:true,
-=======
 import { Schema, model } from "mongoose";
 
 const UserSchema = Schema(
@@ -97,18 +47,12 @@ const UserSchema = Schema(
     },
     {
         timestamps: true,
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
         versionKey: false
     }
 );
 
-<<<<<<< HEAD
-UserSchema.methods.toJSON = function(){
-    const {__v, password, _id, ...usuario}=this.toObject();
-=======
 UserSchema.methods.toJSON = function () {
     const { __v, password, _id, ...usuario } = this.toObject();
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
     usuario.uid = _id;
     return usuario;
 }
