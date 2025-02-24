1fd9ecb2d09d6276f2ccfd76ec5134e328fecafa
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 10:29:38 -0600

    corrección de multiplicación de información

--- src/pet/pet.routes.js
+++ src/pet/pet.routes.js
@@ -1,73 +1,52 @@
-import { Router } from "express";
-import { check } from "express-validator";
-<<<<<<< HEAD
-import { deletePet, getPets, savePet, searchPet, updatePet } from "./pet.controller.js";
-import {validarCampos} from "../middlewares/validar-campos.js";
-import {validarJWT} from "../middlewares/validar-jwt.js";
-=======
-import { savePet, getPets, searchPet, deletePet } from "./pet.controller.js";
-import { validarCampos } from "../middlewares/validar-campos.js"
-import { validarJWT } from '../middlewares/validar-jwt.js'
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
+import { Router } from 'express';
+import { check } from 'express-validator';
+import { savePet, getPets, searchPet, deletePet } from './pet.controller.js';
+import { validarCampos } from '../middlewares/validar-campos.js';
+import { validarJWT } from '../middlewares/validar-jwt.js';
 
 const router = Router();
 
 router.post(
-    "/",
+    '/',
     [
         validarJWT,
-<<<<<<< HEAD
-        check('email','Este no es un correo valido').not().isEmpty(),
-=======
         check('email', 'Este no es un correo valido').not().isEmpty(),
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
         validarCampos
     ],
     savePet
-)
+);
 
-<<<<<<< HEAD
 router.put(
-    "/:id",
+    '/:id',
     [
         validarJWT,
-        check("id","No es un id válido").isMongoId(),
-        check('email','Este no es un correo valido').optional(),
+        check('id', 'No es un id válido').isMongoId(),
+        check('email', 'Este no es un correo valido').optional(),
         validarCampos
     ],
     updatePet
-)
+);
 
-=======
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
-router.get("/", getPets)
+router.get('/', getPets);
 
 router.get(
-    "/:id",
+    '/:id',
     [
         validarJWT,
-<<<<<<< HEAD
-        check("id","No es un id válido").isMongoId(),
-=======
-        check("id", "No es un ID válido").isMongoId(),
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
+        check('id', 'No es un ID válido').isMongoId(),
         validarCampos
     ],
     searchPet
-)
+);
 
 router.delete(
-    "/:id",
+    '/:id',
     [
         validarJWT,
-<<<<<<< HEAD
-        check("id","No es un id válido").isMongoId(),
-=======
-        check("id", "No es un ID válido").isMongoId(),
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
+        check('id', 'No es un ID válido').isMongoId(),
         validarCampos
     ],
     deletePet
-)
+);
 
 export default router;
