4acdcba3e3b7a92adaaf162a03828c8c48dc51ec
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-02-24 09:39:37 -0600

    corrigiendo multiplicación de información

--- src/helpers/db-validator.js
+++ src/helpers/db-validator.js
@@ -1,52 +1,55 @@
-<<<<<<< HEAD
+import { Router } from "express";
+import { login, register, updatePassword } from "./auth.controller.js";
+import { registerValidator, loginValidator, upPassValidator } from "../middlewares/validator.js";
+import { uploadProfilePicture } from "../middlewares/multer-upload.js";
+import { deleteFileOnError } from "../middlewares/delete-file-on-error.js";
+import { validarJWT } from "../middlewares/validar-jwt.js";
 import Role from "../role/role.model.js";
 import User from "../users/user.model.js";
 
-export const esRoleValido = async(role = ' ') => {
-    const existeRol = await Role.findOne({ role });
-
-    if (!existeRol) {
-        throw new Error('El rol ${ role } no existe en la base de datos');
-        
-    }
-}
-export const existenteEmail = async (correo = '')=>{
- 
-    const existeEmail = await User.findOne({correo});
- 
-=======
-import Role from '../role/role.model.js';
-import User from '../users/user.model.js';
+const router = new Router();
 
 export const esRoleValido = async (role = '') => {
-
     const existeRol = await Role.findOne({ role });
-
-    if(!existeRol){
-        throw new Error(`El rol ${ role } no existe en la base de datos`);
+    if (!existeRol) {
+        throw new Error(`El rol ${role} no existe en la base de datos`);
     }
-}
+};
 
-export const existenteEmail = async (correo = ' ') => {
-
+export const existenteEmail = async (correo = '') => {
     const existeEmail = await User.findOne({ correo });
-
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
-    if(existeEmail){
-        throw new Error(`El correo ${ correo } ya existe en la base de datos`);
+    if (existeEmail) {
+        throw new Error(`El correo ${correo} ya existe en la base de datos`);
     }
-}
+};
 
 export const existeUsuarioById = async (id = '') => {
     const existeUsuario = await User.findById(id);
-
-<<<<<<< HEAD
     if (!existeUsuario) {
         throw new Error(`El ID ${id} no existe`);
-        
-=======
-    if(!existeUsuario){
-        throw new Error(`El ID ${id} no existe`);
->>>>>>> 6d17c82c46a7391ce40b773d51e6d5802d8edf50
     }
-}
+};
+
+router.post(
+    '/login',
+    loginValidator,
+    deleteFileOnError,
+    login
+);
+
+router.post(
+    '/register',
+    uploadProfilePicture.single("profilePicture"),
+    registerValidator,
+    deleteFileOnError,
+    register
+);
+
+router.put(
+    '/password/:id',
+    validarJWT,
+    upPassValidator,
+    updatePassword
+);
+
+export default router;
