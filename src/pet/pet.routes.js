8842d6fcbe2c85baa1bd0e61f13c588645df8be7
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-03-10 16:23:30 -0600

    arreglando la multiplicación de información

--- src/pet/pet.routes.js
+++ src/pet/pet.routes.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Router } from 'express';
-import { check } from 'express-validator';
-import { savePet, getPets, searchPet, deletePet } from './pet.controller.js';
-import { validarCampos } from '../middlewares/validar-campos.js';
-import { validarJWT } from '../middlewares/validar-jwt.js';
-
-const router = Router();
-
-router.post(
-    '/',
-    [
-        validarJWT,
-        check('email', 'Este no es un correo valido').not().isEmpty(),
-        validarCampos
-    ],
-    savePet
-);
-
-router.put(
-    '/:id',
-    [
-        validarJWT,
-        check('id', 'No es un id válido').isMongoId(),
-        check('email', 'Este no es un correo valido').optional(),
-        validarCampos
-    ],
-    updatePet
-);
-
-router.get('/', getPets);
-
-router.get(
-    '/:id',
-    [
-        validarJWT,
-        check('id', 'No es un ID válido').isMongoId(),
-        validarCampos
-    ],
-    searchPet
-);
-
-router.delete(
-    '/:id',
-    [
-        validarJWT,
-        check('id', 'No es un ID válido').isMongoId(),
-        validarCampos
-    ],
-    deletePet
-);
-
-export default router;
-=======
-=======
->>>>>>> b7dfb84 (PMA terminado)
 import { Router } from "express";
 import { savePet, getPets, searchPet, deletePet } from "./pet.controller.js";
 import { createPetValidator, getPetByIdValidator, updatePetValidator, deletePetValidator } from "../middlewares/pet-validators.js";
@@ -71,9 +14,5 @@ router.put("/updatePet/:id", updatePetValidator, savePet);
 
 router.delete("/deletePet/:id", deletePetValidator, deletePet);
 
-<<<<<<< HEAD
 export default router;
->>>>>>> 91215ea (PMA terminado)
-=======
-export default router;
->>>>>>> b7dfb84 (PMA terminado)
+
