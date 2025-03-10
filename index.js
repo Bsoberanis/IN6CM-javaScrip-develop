d87e5a6b925358221026bab6a27f540bba5f421b
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-03-10 16:24:52 -0600

    arreglando la multiplicación de información

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { config } from "dotenv";
+import { initServer } from "./configs/server.js";
+
 config();
-
-import { initServer } from './configs/server.js';
 initServer();
-=======
-=======
->>>>>>> b7dfb84 (PMA terminado)
-import { config } from "dotenv"
-import { initServer } from "./configs/server.js"
 
-config()
-<<<<<<< HEAD
-initServer()
->>>>>>> 91215ea (PMA terminado)
-=======
-initServer()
->>>>>>> b7dfb84 (PMA terminado)
