2f4912990201dc823ffcd330c62754dd56632466
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2025-03-10 16:20:50 -0600

    arreglando la multiplicación de información

--- src/middlewares/delete-file-on-error.js
+++ src/middlewares/delete-file-on-error.js
@@ -1,48 +1,32 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import fs from 'fs/promises';
-import { join } from 'path';
-=======
-import fs from "fs/promises";
-import { join } from "path";
->>>>>>> 91215ea (PMA terminado)
-=======
-import fs from "fs/promises";
-import { join } from "path";
->>>>>>> b7dfb84 (PMA terminado)
+import { Router } from "express";
+import { login, register, updatePassword } from "./auth.controller.js";
+import { registerValidator, loginValidator, upPassValidator } from "../middlewares/user-validators.js";
+import { uploadProfilePicture } from "../middlewares/multer-uploads.js";
+import { deleteFileOnError } from "../middlewares/delete-file-on-error.js";
+import { validarJWT } from "../middlewares/validar-jwt.js";
 
-export const deleteFileOnError = async (err, req, res, next) => {
-    if (req.file && req.filePath) {
-        const filePath = join(req.filePath, req.file.filename);
-        try {
-            await fs.unlink(filePath);
-        } catch (unlinkErr) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            console.error('Error deleting file: ', unlinkErr);
-        }
-    }
-    if (err.status === 400 || err.errors) {
-        return res.status(400).json({
-            success: false,
-            errors: err.errors
-        });
-    }
-    return res.status(500).json({
-        success: false,
-        message: err.message
-    });
-}
-=======
-=======
->>>>>>> b7dfb84 (PMA terminado)
-            console.log(`Error deleting file: ${unlinkErr}`);
-        }
-    }
-    next(err);
-<<<<<<< HEAD
-}
->>>>>>> 91215ea (PMA terminado)
-=======
-}
->>>>>>> b7dfb84 (PMA terminado)
+const router = Router();
+
+router.post(
+    "/login",
+    loginValidator,
+    deleteFileOnError,
+    login
+);
+
+router.post(
+    "/register",
+    uploadProfilePicture.single("profilePicture"),
+    registerValidator,
+    deleteFileOnError,
+    register
+);
+
+router.put(
+    "/password/:id",
+    validarJWT,
+    upPassValidator,
+    updatePassword
+);
+
+export default router;
